87fef58414b80ef615394d74a81fef8f5c270485
Tacsipacsi <tacsipacsi@jnet.hu>
2023-02-09 19:37:28 +0100

    Produce valid HTML output
    Add the usual boilerplate. Including the page title, which is not only
    required, but also useful.

--- plot.rb
+++ plot.rb
@@ -8,21 +8,35 @@ Dir.entries('out').sort.each do |d|
 	files << d.sub('.tsv', '')
 end
 
-
-html = '<h1>Usage stats for DiscussionTools and other methods of adding talk page comments</h1>
-<p><strong><a href="https://github.com/MatmaRex/dtstats/blob/master/README.md">Notes and caveats</a></strong></p>
-<p><a href="./data.tgz">Raw data (TSV)</a> &bull; <a href="https://github.com/MatmaRex/dtstats">Source code</a></p>
-'
-
-html += '<ul style="position:fixed; top:0; right:0;">'
-html += files.map{|f|
+toc = files.map{|f|
 	"<li><a href='##{f}'>#{f}</a></li>"
 }.join("\n")
-html += '</ul>'
 
-html += files.map{|f|
-	"<h2 id='#{f}'>#{f}</h2><img src='#{f}.tsv.svg'>"
-}.join("\n")
+graphs = files.map{|f|
+	"<h2 id='#{f}'>#{f}</h2>\n<img src='#{f}.tsv.svg'>"
+}.join("\n\n")
+
+html = <<-HTML
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>DiscussionTools stats</title>
+</head>
+<body>
+<h1>Usage stats for DiscussionTools and other methods of adding talk page comments</h1>
+<p><strong><a href="https://github.com/MatmaRex/dtstats/blob/master/README.md">Notes and caveats</a></strong></p>
+<p><a href="./data.tgz">Raw data (TSV)</a> &bull; <a href="https://github.com/MatmaRex/dtstats">Source code</a></p>
+
+<ul style="position:fixed; top:0; right:0;">
+#{toc}
+</ul>
+
+#{graphs}
+</body>
+</html>
+HTML
+
 
 File.write "plot/index.html", html
 `tar -zcf plot/data.tgz out`
