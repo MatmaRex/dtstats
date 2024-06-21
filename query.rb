43c37cd100be075e861e67473c31e6608709f30c
Bartosz Dziewoński <dziewonski@fastmail.fm>
2024-06-22 01:46:44 +0200

    Update stat server

--- query.rb
+++ query.rb
@@ -7,7 +7,7 @@ end
 def get_command db, file
 	sn = get_sn db
 	mysql = "mysql --defaults-file=/etc/mysql/conf.d/analytics-research-client.cnf -h s#{sn}-analytics-replica.eqiad.wmnet -D #{db} -P 331#{sn} -A"
-	ssh = "ssh stat1006.eqiad.wmnet \"#{mysql}\""
+	ssh = "ssh stat1009.eqiad.wmnet \"#{mysql}\""
 
 	# command = "echo #{sql.gsub "\n", ' '} | #{ssh}"
 	command = "#{ssh} <#{file}"
