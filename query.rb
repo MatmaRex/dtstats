acfff05fd061b7ce99f39f609d68fa96abad8ddc
Bartosz Dziewoński <dziewonski@fastmail.fm>
2024-10-18 04:44:43 +0200

    Use the `analytics-mysql` helper

--- query.rb
+++ query.rb
@@ -1,12 +1,5 @@
-def get_sn db
-	data = File.readlines "C:/mediawiki/operations/mediawiki-config/wmf-config/db-production.php"
-	data = data.drop_while{|a| a !~ /sectionsByDB/ }.take_while{|a| a !~ /\]/ }
-	data.find{|a| a =~ /'#{db}'/ }[/'s(\d+)'/, 1] rescue '3'
-end
-
 def get_command db, file
-	sn = get_sn db
-	mysql = "mysql --defaults-file=/etc/mysql/conf.d/analytics-research-client.cnf -h s#{sn}-analytics-replica.eqiad.wmnet -D #{db} -P 331#{sn} -A"
+	mysql = "analytics-mysql #{db}"
 	ssh = "ssh stat1009.eqiad.wmnet \"#{mysql}\""
 
 	# command = "echo #{sql.gsub "\n", ' '} | #{ssh}"
